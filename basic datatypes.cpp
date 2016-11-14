37dbf1e742746a12c52691beee5ca3181677638f
harsh3081 <harsh30814@gmail.com>
2016-11-14 11:48:29 +0530

    Create basic datatypes.cpp

---
+++ basic datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;
+    long j;
+    long long k;
+    char l;
+    float m;
+    double n;
+    scanf("%d %ld %lld %c %f %lf",&i,&j,&k,&l,&m,&n);
+    
+    printf("%d\n",i);
+    printf("%ld\n",j);
+    printf("%lld\n",k);
+    printf("%c\n",l);
+    printf("%f\n",m);
+    printf("%lf\n",n);
+    // Complete the code.
+    return 0;
+}
